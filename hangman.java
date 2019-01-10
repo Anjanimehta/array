e3f32fbf1d239a571d7535a024269d0024eef080
anjanimehta <anjanimehta99@gmail.com>
2019-01-10 01:33:58 -0800

    init commit

--- hangman.java
+++ hangman.java
@@ -1,6 +1,17 @@
 package main;
 
 public class hangman {
+    public static void main(String[] args) {
+
+
+        System.out.println("enter the secret word ");
+        bufferedReader br= new
+    StringBuffer word;
+
+
+    }
+
+
 
 
 }
